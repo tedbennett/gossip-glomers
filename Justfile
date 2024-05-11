607cee87f7dbc584d8ac14731b0c4c70b640cdf1
Ted Bennett <44608108+tedbennett@users.noreply.github.com>
2024-05-11 12:33:15 +0100

    part 2: guids

--- Justfile
+++ Justfile
@@ -1,2 +1,4 @@
 echo:
 	maelstrom/maelstrom test -w echo --bin target/debug/echo --node-count 1 --time-limit 10	
+guids:
+	maelstrom/maelstrom test -w unique-ids --bin target/debug/guids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
---
+++ src/bin/guids.rs
@@ -0,0 +1,57 @@
+use gossip_glomers::*;
+use std::io::{StdoutLock, Write};
+
+use anyhow::Context;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum GeneratePayload {
+    Generate,
+    GenerateOk { id: String },
+}
+
+struct GenerateNode {
+    id: usize,
+    node_id: String,
+}
+
+impl Node<GeneratePayload> for GenerateNode {
+    fn new(id: usize, init: Init) -> Self {
+        Self {
+            id,
+            node_id: init.node_id,
+        }
+    }
+    fn handle(
+        &mut self,
+        input: Message<GeneratePayload>,
+        output: &mut StdoutLock,
+    ) -> anyhow::Result<()> {
+        match input.body.payload {
+            GeneratePayload::Generate => {
+                let reply = Message {
+                    src: input.dest,
+                    dest: input.src,
+                    body: Body {
+                        id: Some(self.id),
+                        in_reply_to: input.body.id,
+                        payload: GeneratePayload::GenerateOk {
+                            id: format!("{}-{}", self.node_id, self.id),
+                        },
+                    },
+                };
+                self.id += 1;
+                serde_json::to_writer(&mut *output, &reply).context("serializing guid reply")?;
+                output.write_all(b"\n").context("flushing guid reply")?;
+            }
+            GeneratePayload::GenerateOk { .. } => {}
+        }
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    run_loop::<_, GenerateNode>()
+}
